605f25c5e4721138f959b56af8f6546bfc54fcdb
jorge <jorge.siverio@gmail.com>
2024-03-25 01:48:48 -0700

    -

--- Site/test.php
+++ Site/test.php
@@ -3,6 +3,12 @@ require 'Scripts/Classes/User.php';
 require 'Scripts/Include/DBSetup.php';
 require 'Scripts/Include/functions.php';
 
-echo (getUserWaitingForApproval($conn));
+function backupDB($conn): void
+{
+    $backupFile = '/Site/backup/backup-' . date("Y-m-d-H-i-s") . '.sql';
+    $command = "mysqldump --user=root --password=  polarisrms > $backupFile";
+    exec($command);
+    header("Location: ../../adminPanelDB.php?success=Database backed up successfully");
+}
 
-
+backupDB($conn);
